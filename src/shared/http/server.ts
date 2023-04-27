a101b1618a6e111be8b8c437e21a73e787d94a7d
WellingtonStival <wellingtonstival90@gmail.com>
2023-04-27 00:04:56 -0300

    Commit

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import routes from './routes';
 import 'express-async-errors';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
